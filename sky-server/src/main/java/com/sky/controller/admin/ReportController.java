22bbb51229daa0c214ad9faf38895839ea7c5369
SUSUshuo <10819724+susushuo@user.noreply.gitee.com>
2025-03-28 16:24:19 +0800

    feat-统计报表-excel数据导出

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 /**
@@ -48,6 +50,7 @@ public class ReportController {
 
     /**
      * 订单数据统计
+     *
      * @param begin
      * @param end
      * @return
@@ -58,13 +61,14 @@ public class ReportController {
             @DateTimeFormat(pattern = "yyyy-MM-dd")
             LocalDate begin,
             @DateTimeFormat(pattern = "yyyy-MM-dd")
-            LocalDate end){
+            LocalDate end) {
 
-        return Result.success(reportService.getOrderStatistics(begin,end));
+        return Result.success(reportService.getOrderStatistics(begin, end));
     }
 
     /**
      * 销量排名统计
+     *
      * @param begin
      * @param end
      * @return
@@ -73,7 +77,13 @@ public class ReportController {
     @ApiOperation("销量排名统计")
     public Result<SalesTop10ReportVO> top10(
             @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
-            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end){
-        return Result.success(reportService.getSalesTop10(begin,end));
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end) {
+        return Result.success(reportService.getSalesTop10(begin, end));
+    }
+
+    @GetMapping("/export")
+    @ApiOperation("导出运营数据报表")
+    public void export(HttpServletResponse response) {
+        reportService.exportBusinessData(response);
     }
 }
